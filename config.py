58ccf2ccec2701e25818910ea3be0d3dbd5a6b4f
Kesi28 <kschan0428@gmail.com>
2025-12-09 13:21:43 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c56csi8emcov.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "123123123"
+customdb = "employee"
+custombucket = "chankesi-employee"
+customregion = "us-east-1"
+
